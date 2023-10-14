3ff7fd1a495f3e0d8cfd01a8f5bdbf628962c814
Simon Valentino <simontvalentino@gmail.com>
2023-10-14 12:57:56 -0400

    Added db

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(200), nullable=False, unique=True)
+    subject = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.first_name} {self.last_name}"
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
