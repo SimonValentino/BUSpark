6a412f0e060d63b8e97249d9aecb1e73372168ba
Simon Valentino <simontvalentino@gmail.com>
2023-10-18 10:15:49 -0400

    tutor model added

--- core/models.py
+++ core/models.py
@@ -7,3 +7,10 @@ class Student(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=50)
     date_registered = models.DateTimeField(auto_now=True)
+
+class Tutor(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    date_registered = models.DateTimeField(auto_now=True)
