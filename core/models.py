fc99bac9171322e167941f628d8e7e7cbb46e967
Simon Valentino <simontvalentino@gmail.com>
2023-10-19 19:33:02 -0400

    Added base.html

--- core/models.py
+++ core/models.py
@@ -1,16 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Student(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    password = models.CharField(max_length=50)
-    date_registered = models.DateTimeField(auto_now=True)
-
-class Tutor(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    password = models.CharField(max_length=50)
-    date_registered = models.DateTimeField(auto_now=True)
--- sparked/urls.py
+++ sparked/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from core.views import index, signin, signup, student_portal
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("django.contrib.auth.urls")),
     path("", index, name="index"),
     path("signin/", signin, name="signin"),
     path("signup/", signup, name="signup"),
