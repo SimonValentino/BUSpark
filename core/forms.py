5b95189a15464a57f89f823be9bb5245bd7a3d34
Simon Valentino <simontvalentino@gmail.com>
2023-10-20 18:52:38 -0400

    first and lats name required

--- core/forms.py
+++ core/forms.py
@@ -5,6 +5,8 @@ from .models import Review
 
 
 class RegistrationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=50, required=True)
+    last_name = forms.CharField(max_length=50, required=True)
     email = forms.EmailField(required=True)
 
     class Meta:
