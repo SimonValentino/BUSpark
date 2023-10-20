1fff6bd6993b6e34f0bb89910cd397d3754055ae
Simon Valentino <simontvalentino@gmail.com>
2023-10-19 20:55:38 -0400

    Fully function user database

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegistrationForm
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 
@@ -11,6 +12,10 @@ def index(request):
 def sign_up(request):
     if request.method == "POST":
         form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/")
     else:
         form = RegistrationForm()
         
